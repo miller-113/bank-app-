30cf7c04b838f39288895529a545e3fa8072a519
Nikita <nikita.shkuratt@gmail.com>
2024-04-22 17:32:02 +0300

    added btn to business

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] outline-none ${styles}`}
+    >Get Started</button>
+  );
+};
+
+export { Button };
