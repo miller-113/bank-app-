9585ebb95c61b4cc519ae0715cb1ced732c3d156
Nikita <nikita.shkuratt@gmail.com>
2024-04-22 22:23:56 +0300

    upd 6 section gradient

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,6 +8,7 @@ const Testimonials = () => {
       id="clients"
       className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
     >
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"/>
       <div className="w-ful flex justify-between items-center md:flex-row flex-col sm:mb16 mb-6 relative z-[1]">
         <h1 className={styles.heading2}>
           What people are <br className="sm:block hidden" />
